6ec375cee26ce988754dde7d5e11fcef1ebd7bf1
Sergey Petrenko <Stevo98@mail.ru>
2022-11-10 14:32:52 +1000

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        int m = 75;
-        double h = 1.85;
+        int m = 75;// Масса тела в кг
+        double h = 1.85;// Рост в метрах
         int index = service.calculate(m,h);
         System.out.println("Индекс массы тела - " + index);
     }
